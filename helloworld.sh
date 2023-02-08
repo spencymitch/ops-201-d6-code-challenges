ef3568342a00fac1623f99644a5d153bdc1cf98f
spencymitch <spencermitchell409@gmail.com>
2023-02-08 00:59:06 +0000

    Hello world

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Spencer Mitchell
+# Date of latest revision: 2/7/23
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+echo "I am working perfectly"
+
+# End
